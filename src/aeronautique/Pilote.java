8c7c25fcdbfa8dd6503f567b141833aff22e78e8
CoWbOyZ <valentin2.0@gmail.com>
2016-06-08 13:54:33 +0200

    add getter/setter to Pilote.java

--- src/aeronautique/Pilote.java
+++ src/aeronautique/Pilote.java
@@ -19,5 +19,30 @@ public class Pilote {
 				+ ", salaire=" + salaire + "]";
 	}
 	
+	public int getNumeroPilote() {
+		return numeroPilote;
+	}
+	public void setNumeroPilote(int numeroPilote) {
+		this.numeroPilote = numeroPilote;
+	}
+	public String getNomPilote() {
+		return nomPilote;
+	}
+	public void setNomPilote(String nomPilote) {
+		this.nomPilote = nomPilote;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public int getSalaire() {
+		return salaire;
+	}
+	public void setSalaire(int salaire) {
+		this.salaire = salaire;
+	}
+	
 	
 }
